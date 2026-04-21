d09aa2dc7df0f6b17d148594cc9e4ddb8d9880a9
Pragati Gupta <pragatigupta02508@gmail.com>
2026-04-22 00:35:45 +0530

    added backend fresh

---
+++ backend_temp/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.send("No token");
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.send("Invalid token");
+  }
+};
+
+module.exports = auth;
---
+++ backend_temp/server.js
@@ -0,0 +1,61 @@
+require('dotenv').config();   
+
+console.log("Starting server...");
+console.log("MONGO URI:", process.env.MONGO_URI);
+console.log("JWT:", process.env.JWT_SECRET);
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const User = require('./models/User');
+const auth = require('./middleware/auth');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"));
+
+
+// REGISTER
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = new User({ name, email, password: hashed });
+  await user.save();
+
+  res.send("Registered");
+});
+
+
+// LOGIN
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.send("User not found");
+
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+  if (!isMatch) return res.send("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ token });
+});
+
+
+// PROTECTED ROUTE
+app.get('/dashboard', auth, async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
+  res.json(user);
+});
+app.get('/', (req, res) => {
+  res.send("Auth API is running 🚀");
+});
+app.listen(5000, () => console.log("Server running"));
+console.log("MONGO URI:", process.env.MONGO_URI);
